9404d4e467cafb5efc47a3e581326ebb877d5e3e
Robbie <resteinbock@gmail.com>
2016-12-13 21:06:36 -0600

    add test files

--- test/led_pir.js
+++ test/led_pir.js
@@ -2,18 +2,23 @@ let Gpio = require('onoff').Gpio;
 const config = require('../lib/config');
 
 let pir = new Gpio(config.MOTION.PIR_GPIO, 'in', 'both');
+let led = new Gpio(config.MOTION.LED_GPIO, 'out');
 const TIME = 60 * 1000;
 
 pir.watch(function (err, value) {
     if (err) {
-        return pir.unexport();
+        led.unexport();
+        pir.unexport();
+        return;
     }
 
+    led.writeSync(value);
     console.log(value);
 });
 
 setTimeout(
     function() {
+        led.unexport();
         pir.unexport();
     },
     TIME
